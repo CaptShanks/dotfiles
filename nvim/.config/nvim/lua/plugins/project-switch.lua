d6e11005a030ceb4ef67e1075ce9307ac891b4d4
Shashank Tyagi <shashank.tyagi@getinsured.com>
2025-09-04 10:33:14 -0400

    additional folder in project switch

--- nvim/.config/nvim/lua/plugins/project-switch.lua
+++ nvim/.config/nvim/lua/plugins/project-switch.lua
@@ -20,6 +20,9 @@ return {
         "~/Documents/git", -- Added git folder in Documents
         -- Add more directories where you keep git repos
       },
+      additional_folders = {
+        "/Users/sjc-lp03742/Documents/temp",
+      },
       max_depth = 3,
       show_hidden = false, -- Set to true if you want to see repos like .config, .dotfiles
       use_cache = true,
@@ -30,13 +33,39 @@ return {
   end,
   keys = {
     -- Main project switcher
-    { "<leader>fp", function() require("project-switcher").pick_project() end, desc = "Switch Project" },
+    {
+      "<leader>fp",
+      function()
+        require("project-switcher").pick_project()
+      end,
+      desc = "Switch Project",
+    },
 
     -- Refresh project cache
-    { "<leader>fP", function() require("project-switcher").refresh_projects() end, desc = "Refresh Projects" },
+    {
+      "<leader>fP",
+      function()
+        require("project-switcher").refresh_projects()
+      end,
+      desc = "Refresh Projects",
+    },
 
     -- Quick access to common directories (these change nvim's working directory)
-    { "<leader>fh", function() vim.cmd("cd ~"); vim.notify("Changed to home directory") end, desc = "Go to Home" },
-    { "<leader>fC", function() vim.cmd("cd " .. vim.fn.stdpath("config")); vim.notify("Changed to nvim config") end, desc = "Go to Config" },
+    {
+      "<leader>fh",
+      function()
+        vim.cmd("cd ~")
+        vim.notify("Changed to home directory")
+      end,
+      desc = "Go to Home",
+    },
+    {
+      "<leader>fC",
+      function()
+        vim.cmd("cd " .. vim.fn.stdpath("config"))
+        vim.notify("Changed to nvim config")
+      end,
+      desc = "Go to Config",
+    },
   },
 }
