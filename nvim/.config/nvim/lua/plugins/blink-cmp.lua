8bd4a4db47b33797105dced519854864cecc4d4b
Shashank Tyagi <shashank.tyagi@getinsured.com>
2025-10-29 21:16:11 -0400

    fix(nvim): migrate blink-cmp to new LuaSnip preset system

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -36,7 +36,7 @@ return {
     },
 
     sources = {
-      default = { "lsp", "path", "luasnip", "buffer", "copilot" },
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
       providers = {
         copilot = {
           name = "copilot",
@@ -86,18 +86,7 @@ return {
     },
 
     snippets = {
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
+      preset = "luasnip",
     },
 
 
